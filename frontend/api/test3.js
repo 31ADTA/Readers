12b5728bcc9b4bf84bacda90943e2d666629f42d
ADITYA TALE <adityatale3107@gmail.com>
2026-03-11 08:49:10 +0530

    Delete frontend/api/test3.js

--- frontend/api/test3.js
+++
@@ -1,5 +0,0 @@
-const map = { eng: "en", hin: "hi", fre: "fr", ger: "de", spa: "es", ita: "it", por: "pt", rus: "ru", chi: "zh", jpn: "ja", ara: "ar", mar: "mr", ben: "bn" };
-const tests = ["eng", "chi", "por", "fre"];
-for (let lang of tests) {
-  console.log(map[lang] || lang.substring(0,2));
-}
