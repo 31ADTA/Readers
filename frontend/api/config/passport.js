87d203bb497285c27c2cf5aedda23b810748d07a
ADITYA TALE <adityatale3107@gmail.com>
2026-03-11 13:15:34 +0530

    Update passport.js

--- frontend/api/config/passport.js
+++ frontend/api/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
+  callbackURL: 'https://readers-git-main-rockys-projects-3654e599.vercel.app/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if user already exists
